398f5a71da107cff0c91d83deb746dca22d68b95
unknown <jordanbeziau@gmail.com>
2017-04-06 16:58:31 +0200

    Class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
+class Formation {
+  constructor(nom, eleves) {
+    this.nom = nom;
+    this.eleves = eleves;
+  }
+  get_eleves() {
+    console.log(`Voici les élèves de la formation ${this.nom} :`);
+    this.eleves.forEach((elem, ind) => {
+      console.log(`${ind} -> ${elem}`);
+    });
+  }
+}
 
-  
+class FormationJS extends Formation {
+  constructor(nom, prof, eleves) {
+    super(nom, eleves);
+    this.prof = prof;
+  }
+  get_prof() {
+    console.log(`Le prof s'appelle ${this.prof}`);
+  }
+}
 
-});
+let formation = new FormationJS('test', 'Pascal', ['Loic', 'Mathieu', 'Nicolas']);
+
+debugger
